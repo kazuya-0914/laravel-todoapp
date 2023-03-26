338fd9e5f8eb772a962d3c8848c40baf46f7e533
kazuya-0914 <kazu.work.0914@gmail.com>
2023-03-27 08:00:33 +0900

    3/27AMコミット

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\GoalController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::get('/', [GoalController::class, 'index'])->name('index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
