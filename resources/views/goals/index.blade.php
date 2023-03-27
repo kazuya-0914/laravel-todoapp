5c57819a29fab7704606cdf84de8c5fb6bc56ea8
kazuya-0914 <kazu.work.0914@gmail.com>
2023-03-27 17:16:00 +0900

    3/27PMコミット

--- resources/views/goals/index.blade.php
+++ resources/views/goals/index.blade.php
@@ -1,12 +1,15 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('ToDoアプリ') }}
+        </h2>
+    </x-slot>
+
+    <section class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <p class="p-6 text-gray-900">TESTです</p>
+            </div>
+        </div>
+    </section>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 //Route::get('/', function () {
 //    return view('welcome');
 //});
-Route::get('/', [GoalController::class, 'index'])->name('index');
+Route::get('/', [GoalController::class, 'index'])->middleware(['auth', 'verified'])->name('index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
