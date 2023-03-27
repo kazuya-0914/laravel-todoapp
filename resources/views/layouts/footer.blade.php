95e36fd75bccabee66cd4ee8b4cea281851f5f02
kazuya-0914 <kazu.work.0914@gmail.com>
2023-03-27 23:16:32 +0900

    3/27夜コミット

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="w-full bg-white mt-auto">
+<footer class="w-full bg-white fixed bottom-0">
     <div class="h-16 flex justify-center items-center">
         <p class="text-sm">&copy; {{ config('app.name', 'Laravel') }} All rights reserved.</p>
     </div>
